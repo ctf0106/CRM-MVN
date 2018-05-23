48ebd9c02a03af35ba27a89a7dc9f6e38c613196
ctf0106 <ctf0106@163.com>
2018-05-23 14:19:34 +0800

    数据库修改

--- db_crm.sql
+++ db_crm.sql
@@ -1,19 +1,35 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50528
+Source Server         : loccalhost
+Source Server Version : 50640
 Source Host           : localhost:3306
 Source Database       : db_crm
 
 Target Server Type    : MYSQL
-Target Server Version : 50528
+Target Server Version : 50640
 File Encoding         : 65001
 
-Date: 2018-05-23 01:21:22
+Date: 2018-05-23 14:18:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_company
+-- ----------------------------
+DROP TABLE IF EXISTS `t_company`;
+CREATE TABLE `t_company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `address` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_company
+-- ----------------------------
+INSERT INTO `t_company` VALUES ('1', '公司测试', '公司地址');
 
 -- ----------------------------
 -- Table structure for t_customer
@@ -26,20 +42,15 @@ CREATE TABLE `t_customer` (
   `address` varchar(500) DEFAULT NULL,
   `postCode` varchar(50) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
-  `state` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_customer
 -- ----------------------------
-INSERT INTO `t_customer` VALUES ('1', 'KH21321321', '北京大牛科技', '北京海淀区双榆树东里15号', '100027', '010-62263393', '0');
-INSERT INTO `t_customer` VALUES ('16', 'KH20150526073022', '风驰科技', '321', '21', '321', '1');
-INSERT INTO `t_customer` VALUES ('17', 'KH20150526073023', '巨人科技', null, null, null, '1');
-INSERT INTO `t_customer` VALUES ('18', 'KH20150526073024', '新人科技', null, null, null, null);
-INSERT INTO `t_customer` VALUES ('19', 'KH20150526073025', '好人集团', null, null, null, null);
-INSERT INTO `t_customer` VALUES ('20', 'KH20150526073026', '新浪', null, null, null, null);
-INSERT INTO `t_customer` VALUES ('21', 'KH20150526073027', '百度', null, null, null, null);
+INSERT INTO `t_customer` VALUES ('16', 'KH20150526073022', '风驰科技', '321', '21', '321');
+INSERT INTO `t_customer` VALUES ('30', 'KH20180523015748', '风驰科技', '321', null, null);
+INSERT INTO `t_customer` VALUES ('29', 'KH20180523012220', 'dddd', 'ddd', '1111', '1111111');
 
 -- ----------------------------
 -- Table structure for t_user
