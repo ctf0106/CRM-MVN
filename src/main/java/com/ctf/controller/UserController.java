fa5d81b70acd608c4228e358216a92dfa8effcfb
ctf0106 <ctf0106@163.com>
2018-05-23 14:14:36 +0800

    用户代码删除

--- src/main/java/com/ctf/controller/UserController.java
+++ src/main/java/com/ctf/controller/UserController.java
@@ -56,48 +56,6 @@ public class UserController {
 	}
 	
 	/**
-	 * 分页条件查询用户
-	 * @param page
-	 * @param rows
-	 * @param s_user
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/list")
-	public String list(@RequestParam(value="page",required=false)String page,@RequestParam(value="rows",required=false)String rows,User s_user,HttpServletResponse response)throws Exception{
-		PageBean pageBean=new PageBean(Integer.parseInt(page),Integer.parseInt(rows));
-		Map<String,Object> map=new HashMap<String,Object>();
-		map.put("userName", StringUtil.formatLike(s_user.getUserName()));
-		map.put("start", pageBean.getStart());
-		map.put("size", pageBean.getPageSize());
-		List<User> userList=userService.find(map);
-		Long total=userService.getTotal(map);
-		JSONObject result=new JSONObject();
-		JSONArray jsonArray=JSONArray.fromObject(userList);
-		result.put("rows", jsonArray);
-		result.put("total", total);
-		ResponseUtil.write(response, result);
-		return null;
-	}
-	
-	/**
-	 * 获取客户经理信息 下拉框数据用到
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/customerManagerComboList")
-	public String customerManagerComboList(HttpServletResponse response)throws Exception{
-		Map<String,Object> map=new HashMap<String,Object>();
-		map.put("roleName", "客户经理");
-		List<User> userList=userService.find(map);
-		JSONArray row=JSONArray.fromObject(userList);
-		ResponseUtil.write(response, row);
-		return null;
-	}
-	
-	/**
 	 * 添加或者修改用户
 	 * @param user
 	 * @param response
@@ -118,25 +76,6 @@ public class UserController {
 		}else{
 			result.put("success", false);
 		}
-		ResponseUtil.write(response, result);
-		return null;
-	}
-	
-	/**
-	 * 删除用户
-	 * @param ids
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/delete")
-	public String delete(@RequestParam(value="ids")String ids,HttpServletResponse response)throws Exception{
-		String []idsStr=ids.split(",");
-		for(int i=0;i<idsStr.length;i++){
-			userService.delete(Integer.parseInt(idsStr[i]));
-		}
-		JSONObject result=new JSONObject();
-		result.put("success", true);
 		ResponseUtil.write(response, result);
 		return null;
 	}
